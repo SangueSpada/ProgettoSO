23320411f58f17b2eb86b817753fa3af2301518d
Sanguespada <kankilic.1888465@studenti.uniroma1.it>
2022-10-21 18:27:32 +0200

    new pare and cpu usage

--- top.c
+++ top.c
@@ -6,12 +6,98 @@
 #include <fnmatch.h>
 #include <sys/types.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
 
 #define BUFSZ 1024
 #define MEM_FIELD_NAME "VmSize:"
 
+long Hertz=0;
+long uptime=0;
+
+
+char * concat(char* s1, char c){
+
+
+char * res=(char*) malloc((strlen(s1)+2)*sizeof(char));
+
+int i;
+
+for(i=0;i<strlen(s1);i++){
+    res[i]= s1[i];
+}
+
+res[strlen(s1)]=c;
+res[strlen(s1)+1]='\0';
+
+return res;
+
+
+}
+
+
+
+
+long get_uptime(){
+
+    struct sysinfo s_info;
+    int err= sysinfo(&s_info);
+    if(err!=0){
+        printf("errore lettura uptime, code= %d\n", err);
+        return -1;
+    }
+
+    return s_info.uptime;
+
+
+}
+
+
+char** split_string(char *line, int* s) {
+
+    //char* res[100];
+    char** res=(char **)malloc(100*sizeof(char*));
+
+    int index=0;
+    const char delimiter[] = " ";
+    char * tmp;
+
+    tmp = strtok(line, delimiter);
+    if (tmp == NULL){
+    return NULL;
+    } 
+
+    res[index]=tmp;
+    index++;
+    //printf("%s\n", tmp);
+    tmp = strtok(NULL, ")");
+
+    char * d=concat(tmp,')');
+
+    res[index]=d;
+    index++;
+
+
+
+    for (;;) {
+        tmp = strtok(NULL, delimiter);
+        if (tmp == NULL){
+            *s=index;
+            return res;
+        }
+        
+        res[index]=tmp;
+        index++;
+        //printf("%s\n", tmp);
+    }
+}
+
 void processdir(const struct dirent *piddir)
 {
+
+    //printf("%s\n",piddir->d_name);
+    char *lines = NULL;
+    size_t size;
     char path[BUFSZ];
     char line[BUFSZ];
     char *memstr;
@@ -22,12 +108,39 @@ void processdir(const struct dirent *piddir)
     sprintf(filename, "/proc/%s/stat", piddir->d_name);
     FILE *f = fopen(filename, "r");
 
-    int unused;
-    char comm[1000];
-    char state;
-    int ppid;
-    fscanf(f, "%d %s %c %d", &unused, comm, &state, &ppid);
-    printf("%s\t%c\t%s\n", piddir->d_name,state,comm);
+
+    char** data;
+    int s=0;
+
+    while (getline(&lines, &size, f) != -1) {
+        data=split_string(lines, &s);
+    }
+
+    //printf("size= %d\n",s);
+    /*for(int i=0;i<s;i++){
+
+        printf("%s\n",data[i]);
+
+    }*/
+
+
+
+
+    float total_time= atoi(data[13])+atoi(data[14])+atoi(data[15])+atoi(data[16]);
+
+    float seconds= uptime - (atoi(data[21])/Hertz);
+
+    
+    float cpu_usage= 100 *( (total_time/Hertz)/seconds);
+
+
+
+
+
+    printf("%s\t%s\t%-7.2f\t\t\t%s\n", piddir->d_name,data[2],cpu_usage, data[1]);
+
+    free(data[1]);
+    free(data);
     fclose(f);
     
 }
@@ -38,12 +151,19 @@ int main()
      DIR *procdir;
      struct dirent *procentry;
 
+     Hertz= sysconf(_SC_CLK_TCK);
+     uptime=get_uptime();
+
+     if(uptime==-1){
+        return -1;
+     }
+
      procdir = opendir("/proc");
      if (procdir == NULL) {
          perror("Could not open directory /proc");
          return 1;
      }
-     printf("PPID\tSTATE\tCOMMAND\n");
+     printf("PPID\tSTATE\tCPU_USAGE(perc.)\t\t\tCOMMAND\n");
 
      while(1) {
          procentry = readdir(procdir);
