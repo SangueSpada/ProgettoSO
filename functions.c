842b70760d3d17ab594af1dadf961685db8186e5
fracchiooo <frasconocchia1@gmail.com>
2022-10-31 15:36:18 +0100

    functions.h creato

---
+++ functions.c
@@ -0,0 +1,112 @@
+
+#include "functions.h"
+#include <sys/sysinfo.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char* states(int pid, Process_t *process){
+    char file_n[30];
+    char n_pid[10];
+    char* status;
+    char* err = NULL;
+    sprintf(n_pid, "%d", pid); //int to string
+    int res=sprintf(file_n, "/proc/%d/stat", pid);
+    
+    FILE *f = fopen(file_n, "r");
+    if(f==NULL){
+        printf("errore apertura file stat");
+        return err;
+    }
+
+    res=fscanf(f, "%*d %*s %s", status); //evitiamo il pid e il command
+    if(res==-1){
+        printf("errore lettura status");
+        return err;
+    }
+
+    return status;
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+char** split_string(char *line, int* s) {
+
+    char** res=(char **)malloc(100*sizeof(char*));
+
+    int index=0;
+    const char* delimiter = " ";
+    char * tmp;
+
+
+
+
+    tmp = strtok(line, delimiter);
+    if (tmp == NULL){
+    return NULL;
+    } 
+
+    res[index]=tmp;
+    index++;
+    //printf("%s\n", tmp);
+    
+    char* p=line;
+    char* ress=(char*)malloc(30*sizeof(char));
+    int iii=0;
+    while(p!=NULL){
+        if(*p=='('){
+            //tmp = strtok(NULL,"(");
+            ress[iii]='(';
+            iii++;
+            p++;
+            while(!(*p==')'&& *(p+1)==' ')){
+                ress[iii]=*p;
+                iii++;
+                p++;
+
+            }
+            ress[iii]=')';
+            p+=2;
+            ress[iii+1]='\0';
+            break;
+        }
+        p++;
+    }    
+
+
+    res[index]=ress;
+    index++;
+
+    tmp=strtok(p,delimiter);
+    res[index]=tmp;
+    index++;
+
+    for (;;) {
+        tmp = strtok(NULL, delimiter);
+        if (tmp == NULL){
+            *s=index; //size della res
+            return res;
+        }
+        
+        res[index]=tmp;
+        index++;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+long get_uptime(){
+    struct sysinfo s_info;
+    int err= sysinfo(&s_info);
+    if(err!=0){
+        printf("errore lettura uptime, code= %d\n", err);
+        return -1;
+    }
+    return s_info.uptime;
+}
--- top.c
+++ top.c
@@ -7,10 +7,9 @@
 #include <sys/types.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/sysinfo.h>
 #include <signal.h>
 #include <errno.h>
-
+#include "functions.h"
 #include "process.h"
 
 //gloabal variables
@@ -21,114 +20,6 @@ Process_t* p = NULL;
 
 
 
-
-char* states(int pid, Process_t *process){
-    char file_n[30];
-    char n_pid[10];
-    char* status;
-    char* err = NULL;
-    sprintf(n_pid, "%d", pid); //int to string
-    int res=sprintf(file_n, "/proc/%d/stat", pid);
-    
-    FILE *f = fopen(file_n, "r");
-    if(f==NULL){
-        printf("errore apertura file stat");
-        return err;
-    }
-
-    res=fscanf(f, "%*d %*s %s", status); //evitiamo il pid e il command
-    if(res==-1){
-        printf("errore lettura status");
-        return err;
-    }
-
-    return status;
-
-}
-
-char * concat(char* s1, char c){
-    char * res=(char*) malloc((strlen(s1)+2)*sizeof(char));
-    int i;
-    for(i=0;i<strlen(s1);i++){
-        res[i]= s1[i];
-    }
-    res[strlen(s1)]=c;
-    res[strlen(s1)+1]='\0';
-    return res;
-}
-
-char** split_string(char *line, int* s) {
-
-    char** res=(char **)malloc(100*sizeof(char*));
-
-    int index=0;
-    const char* delimiter = " ";
-    char * tmp;
-
-
-
-
-    tmp = strtok(line, delimiter);
-    if (tmp == NULL){
-    return NULL;
-    } 
-
-    res[index]=tmp;
-    index++;
-    //printf("%s\n", tmp);
-    
-    char* p=line;
-    char* ress=(char*)malloc(30*sizeof(char));
-    int iii=0;
-    while(p!=NULL){
-        if(*p=='('){
-            //tmp = strtok(NULL,"(");
-            ress[iii]='(';
-            iii++;
-            p++;
-            while(!(*p==')'&& *(p+1)==' ')){
-                ress[iii]=*p;
-                iii++;
-                p++;
-
-            }
-            ress[iii]=')';
-            p+=2;
-            ress[iii+1]='\0';
-            break;
-        }
-        p++;
-    }    
-
-
-    res[index]=ress;
-    index++;
-
-    tmp=strtok(p,delimiter);
-    res[index]=tmp;
-    index++;
-
-    for (;;) {
-        tmp = strtok(NULL, delimiter);
-        if (tmp == NULL){
-            *s=index; //size della res
-            return res;
-        }
-        
-        res[index]=tmp;
-        index++;
-    }
-}
-
-long get_uptime(){
-    struct sysinfo s_info;
-    int err= sysinfo(&s_info);
-    if(err!=0){
-        printf("errore lettura uptime, code= %d\n", err);
-        return -1;
-    }
-    return s_info.uptime;
-}
 
 
 void processdir(const struct dirent *piddir)
@@ -232,10 +123,12 @@ void processdir(const struct dirent *piddir)
         else if(state == 2){
 
             char* stato=states(pid,p);
+            
             printf("il processo %d ha stato: %s! \n",pid,stato);
 
 
             if(strcmp("suspend",comm)==0){
+
 
                 if(*stato=='R'){
                     int res = kill((pid_t)pid,SIGSTOP);
